fb6f731073672981092b1d258ed5b37628a3f886
MrAladawee <91622707+MrAladawee@users.noreply.github.com>
2022-10-17 11:35:32 +0300

    Create main.cs

---
+++ task4/main.cs
@@ -0,0 +1,14 @@
+using System;
+using Matrix_space;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix m = new Matrix(3, 4);
+            m.show();
+        }
+    }
+}
