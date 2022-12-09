1f177fbb43f6512867ba79bfd407fd757ef0e04f
MrAladawee <91622707+MrAladawee@users.noreply.github.com>
2022-12-09 20:08:50 +0300

    Update main.cs

--- task5/main.cs
+++ task5/main.cs
@@ -1,1 +1,15 @@
-// Code will be here
+using TaskWithRazrMatrix;
+using System.Collections;
+
+DischargedMatrix<int> test = new DischargedMatrix<int>(4, 5);
+
+test[2, 2] = 4;
+test[0, 2] = 5;
+test[0, 3] = 6;
+test[3, 4] = 7;
+test[1, 2] = 8;
+test[0, 0] = 9;
+
+test.show();
+Console.WriteLine();
+test.sq(0, 3).show();
